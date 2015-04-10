6e2d0203060c234c096cc8a9351ce59be8a9b13a
Jace Ju <jaceju@gmail.com>
2015-04-10 18:49:58 +0800

    Create full task for installation.

--- Vagrantfile
+++ Vagrantfile
@@ -4,13 +4,9 @@ VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
-  config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
-
-  config.vm.define "phpci" do |phpci|
-    phpci.vm.box = "ubuntu-14.04-lamp"
-    phpci.vm.provision :shell, :path => "./assets/init.sh"
-    phpci.vm.network "forwarded_port", guest: 80, host: 8888
-    phpci.vm.network "private_network", ip: "192.168.10.20"
-  end
+    config.vm.box = "ubuntu/trusty64"
+    config.vm.provision :shell, :path => "./assets/init.sh"
+    config.vm.network "forwarded_port", guest: 80, host: 8888
+    config.vm.network "private_network", ip: "192.168.10.20"
 
 end
--- assets/init.sh
+++ assets/init.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
 
+# Locale
+sudo locale-gen en_US.UTF-8
+REP1='LANGUAGE="en_US.UTF-8"\nLC_ALL="en_US.UTF-8"\nLC_CTYPE="en_US.UTF-8"\nLANG="en_US.UTF-8"'
+sudo echo -e $REP1|cat /etc/environment - > /tmp/out && sudo mv /tmp/out /etc/environment
+
+# PHP
+sudo echo "deb http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu trusty main" > /etc/apt/sources.list.d/ondrej-php5-5_6-trusty.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
+sudo apt-get update
+sudo apt-get -y install php5 php5-ldap php5-mcrypt php5-gearman php5-memcached php5-gd php5-imagick php5-geoip php5-mysql php5-sqlite php5-xdebug
+
+# MySQL (after PHP)
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password secret'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password secret'
+sudo apt-get -y install mysql-server
+
+# Git
+sudo apt-get -y install git
+
+# Apache (after PHP)
+sudo a2enmod rewrite
+
+# Composer (after PHP)
+sudo cp /vagrant/assets/composer.phar /usr/local/bin/composer
+sudo chmod +x /usr/local/bin/composer
+sudo composer self-update
+
 # Database
 mysql -uroot -psecret < /vagrant/assets/phpci.sql
 
@@ -23,10 +50,10 @@ then
 fi
 
 # PHPCI
-tar xvzf assets/phpci.tar.gz phpci
-cd phpci && git pull
+tar xzf assets/phpci.tar.gz phpci
+cd phpci
 ./console phpci:install \
-    --url="http://phpci.local" \
+    --url="//phpci.local" \
     --db-host=localhost \
     --db-name=phpci \
     --db-user=phpci \
@@ -34,9 +61,24 @@ cd phpci && git pull
     --admin-name=admin \
     --admin-pass=secret \
     --admin-mail=admin@example.com
+mv .htaccess.dist .htaccess
+
 
 # Site
+cd /etc/apache2
+REP2='ServerName phpci.local\nListen 80'
+sudo sed -e "s/Listen 80$/$(echo $REP2)/" /etc/apache2/ports.conf > /tmp/out && sudo mv /tmp/out /etc/apache2/ports.conf
+
 cd /etc/apache2/sites-available
 sudo rm -f *.conf
 cp /vagrant/assets/*.conf .
+
+# SSL Key
+sudo mkdir /etc/apache2/ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -subj "/C=TW/ST=Taiwan/L=Taipei/O=Home/OU=Web/CN=phpci.local" \
+    -keyout /etc/apache2/ssl/apache.key \
+    -out /etc/apache2/ssl/apache.crt
+sudo a2enmod ssl
+sudo a2ensite default-ssl
 sudo service apache2 restart
