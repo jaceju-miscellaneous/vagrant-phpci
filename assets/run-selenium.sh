7c669fdaa647940e710fe89b2737bc01a18c9cd7
Jace Ju <jaceju@gmail.com>
2015-05-27 12:55:39 +0800

    Add selenium script

---
+++ assets/run-selenium.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+BASE_PATH="/Users/jaceju/Vagrant/vagrant-phpci/assets"
+cd $BASE_PATH
+java -jar ${BASE_PATH}/selenium-server-standalone-2.45.0.jar \
+	-firefoxProfileTemplate ${BASE_PATH}/firefox-profile \
+	-log ${BASE_PATH}/selenium.log \
+	-hub http://127.0.0.1:4444/grid/register
