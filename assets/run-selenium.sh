4bab999cc9ed9d87133bbe9f8ee61c4617ecdde2
Jace Ju <jaceju@gmail.com>
2015-05-27 17:43:03 +0800

    Fix hub path of selenium testing script

--- assets/run-selenium.sh
+++ assets/run-selenium.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+
 BASE_PATH="/Users/jaceju/Vagrant/vagrant-phpci/assets"
-cd $BASE_PATH
+
 java -jar ${BASE_PATH}/selenium-server-standalone-2.45.0.jar \
 	-firefoxProfileTemplate ${BASE_PATH}/firefox-profile \
 	-log ${BASE_PATH}/selenium.log \
-	-hub http://127.0.0.1:4444/grid/register
+	-hub http://127.0.0.1:4444/wd/hub
