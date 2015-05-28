54ad811761b3e8b09f1a87eeacced2c6077a6572
Jace Ju <jaceju@gmail.com>
2015-05-28 11:52:12 +0800

    Create service script of selenium server

--- assets/run-selenium-server.sh
+++
@@ -1,4 +0,0 @@
-## Run Xvfb with selenium server
-Xvfb :0 -screen 0 1024x768x24 2>&1 >/dev/null &
-export DISPLAY=:0
-nohup xvfb-run java -jar /vagrant/assets/selenium-server-standalone-2.45.0.jar -log selenium.log -firefoxProfileTemplate "/vagrant/assets/firefox-profile" &
