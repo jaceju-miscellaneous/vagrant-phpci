9f8c3c5961ea2f43cb2b46f8093b744505a84b58
Jace Ju <jaceju@gmail.com>
2015-05-28 15:42:14 +0800

    Move the files related to selenium to another repo

--- assets/run-phantomjs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup phantomjs --webdriver=4444 &
--- assets/selenium-test/tests/acceptance/GitHubCept.php
+++
@@ -1,5 +0,0 @@
-<?php
-$I = new WebGuy($scenario);
-$I->wantTo('see GitHub word in title ');
-$I->amOnPage('/');
-$I->seeInTitle('GitHub');
