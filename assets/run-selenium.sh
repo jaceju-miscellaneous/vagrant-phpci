d2ede2c22b8d4690f553e2c4efb15c731204eeec
Jace Ju <jaceju@gmail.com>
2015-05-28 11:51:44 +0800

    Remove test script

--- assets/run-selenium.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-BASE_PATH="/Users/jaceju/Vagrant/vagrant-phpci/assets"
-
-java -jar ${BASE_PATH}/selenium-server-standalone-2.45.0.jar \
-	-firefoxProfileTemplate ${BASE_PATH}/firefox-profile \
-	-log ${BASE_PATH}/selenium.log \
-	-hub http://127.0.0.1:4444/wd/hub
