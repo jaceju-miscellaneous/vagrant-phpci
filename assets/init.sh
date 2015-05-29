3904c1288de5725e57e3e650401b442388a65365
Jace Ju <jaceju@gmail.com>
2015-05-29 15:49:10 +0800

    Move position of command of add-key and source list

--- assets/init.sh
+++ assets/init.sh
@@ -15,22 +15,33 @@ sudo apt-get -y -qq install automake libtool "g++" \
     libsqlite3-dev libfontconfig1-dev libicu-dev libfreetype6 libssl-dev \
     libpng-dev libjpeg-dev python
 
+# Add Keys & source list
+## PHP
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
+sudo sh -c 'echo "deb http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu trusty main" > /etc/apt/sources.list.d/ondrej-php5-5_6-trusty.list'
+## PhantomJS
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9CE6C37ED6243D66
+sudo sh -c 'echo "deb http://ppa.launchpad.net/tanguy-patte/phantomjs/ubuntu trusty main" > /etc/apt/sources.list.d/phantomjs.list'
+sudo sh -c 'echo "deb-src http://ppa.launchpad.net/tanguy-patte/phantomjs/ubuntu trusty main" >> /etc/apt/sources.list.d/phantomjs.list'
+## Firefox
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
+sudo sh -c 'echo "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu trusty main" >> /etc/apt/sources.list.d/mozilla-security.list'
+sudo sh -c 'echo "deb-src http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu trusty main" >> /etc/apt/sources.list.d/mozilla-security.list'
+## Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+## Update
+sudo apt-get -qq update
+
 # Locale
 sudo locale-gen en_US.UTF-8
 REP1='LANGUAGE="en_US.UTF-8"\nLC_ALL="en_US.UTF-8"\nLC_CTYPE="en_US.UTF-8"\nLANG="en_US.UTF-8"'
 sudo echo -e $REP1|cat /etc/environment - > /tmp/out && sudo mv /tmp/out /etc/environment
 
 # PHP
-sudo sh -c 'echo "deb http://ppa.launchpad.net/ondrej/php5-5.6/ubuntu trusty main" > /etc/apt/sources.list.d/ondrej-php5-5_6-trusty.list'
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-sudo apt-get -qq update
 sudo apt-get -y -qq install php5 php5-curl php5-ldap php5-mcrypt php5-gearman php5-memcached php5-gd php5-imagick php5-geoip php5-mysql php5-sqlite php5-xdebug
 
 # PhantomJS
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9CE6C37ED6243D66
-sudo sh -c 'echo "deb http://ppa.launchpad.net/tanguy-patte/phantomjs/ubuntu trusty main" > /etc/apt/sources.list.d/phantomjs.list'
-sudo sh -c 'echo "deb-src http://ppa.launchpad.net/tanguy-patte/phantomjs/ubuntu trusty main" >> /etc/apt/sources.list.d/phantomjs.list'
-sudo apt-get -qq update
 sudo apt-get -y -qq install phantomjs
 
 # MySQL (after PHP)
@@ -47,7 +58,7 @@ sudo apt-get -y -qq install nodejs
 
 # Selenium & Xvfb
 ## @see http://mike.ucoz.com/publ/programming/ubuntu/selenium_ubuntu_amazon_ec2_headless/8-1-0-4
-## Install headless java & fonts & xvfb & xserver (important) & firefox
+## Install headless java & fonts & xvfb & xserver (important)
 sudo apt-get -y -qq install openjdk-7-jre-headless \
                             xfonts-100dpi xfonts-75dpi \
                             xfonts-scalable xfonts-cyrillic \
@@ -55,19 +66,27 @@ sudo apt-get -y -qq install openjdk-7-jre-headless \
 
 # Firefox
 ## install dbus-x11 to fix "//bin/dbus-launch terminated abnormally without any error message"
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
-sudo sh -c 'echo "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu trusty main" >> /etc/apt/sources.list.d/mozilla-security.list'
-sudo sh -c 'echo "deb-src http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu trusty main" >> /etc/apt/sources.list.d/mozilla-security.list'
-sudo apt-get -qq update
 sudo apt-get -y -qq install dbus-x11 firefox
-firefox -CreateProfile testing
 
 # Google Chrome
 # @see http://www.howopensource.com/2011/10/install-google-chrome-in-ubuntu-11-10-11-04-10-10-10-04/
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get -qq update
 sudo apt-get -y -qq install google-chrome-stable
+
+# OperaDriver
+wget -N https://github.com/operasoftware/operachromiumdriver/releases/download/v0.2.2/operadriver_linux64.zip
+unzip operadriver_linux64.zip
+rm -f operadriver_linux64.zip
+chmod +x operadriver
+sudo mv operadriver /usr/local/share/
+sudo ln -s /usr/local/share/operadriver /usr/bin/operadriver
+
+# ChromeDriver
+wget -N http://chromedriver.storage.googleapis.com/2.10/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+rm -f chromedriver_linux64.zip
+chmod +x chromedriver
+sudo mv chromedriver /usr/local/share/
+sudo ln -s /usr/local/share/chromedriver /usr/bin/chromedriver
 
 # Libsass
 git clone https://github.com/sass/libsass.git
